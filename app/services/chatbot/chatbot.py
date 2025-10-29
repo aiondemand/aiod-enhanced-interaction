83b74b32e472fbf12667298f17567ec8bc2f9628
krupp <lars.krupp@dfki.de>
2025-10-29 09:11:20 +0100

    updated url pathing in Chatbot to lead to mylibrary resources when possible

--- app/services/chatbot/chatbot.py
+++ app/services/chatbot/chatbot.py
@@ -168,6 +168,28 @@ async def get_past_conversation_messages(conversation_id: str):
     )
 
 
+def generate_url(asset_type, asset_id):
+    asset_id = asset_id
+    if asset_type == "datasets":
+        url = f"https://mylibrary.aiod.eu/resources/data_{asset_id}?category=Dataset"
+    elif asset_type == "ml_models":
+        url = f"https://mylibrary.aiod.eu/resources/mdl_{asset_id}?category=AIModel"
+    elif asset_type == "publications":
+        url = f"https://mylibrary.aiod.eu/resources/pub_{asset_id}?category=Publication"
+    elif asset_type == "case_studies":
+        url = f"https://mylibrary.aiod.eu/resources/case_{asset_id}?category=Case%20studies"
+    elif asset_type == "educational_resources":
+        url = f"https://mylibrary.aiod.eu/resources/edu_{asset_id}?category=Educational%20resource"
+    elif asset_type == "experiments":
+        url = f"https://mylibrary.aiod.eu/resources/exp_{asset_id}?category=Experiment"
+    # elif asset_type == "services":
+    #    url = f"https://mylibrary.aiod.eu/resources/exp_{asset_id}?category=Experiment"
+    # todo how is the url for services built
+    else:
+        url = None
+    return url
+
+
 def _asset_search(query: str, asset: str) -> str:
     embedding_vector = EMBEDDING_MODEL.compute_query_embeddings(query)[0]
     mapped_asset = map_asset_name(asset)
@@ -203,12 +225,15 @@ def _asset_search(query: str, asset: str) -> str:
             seen_asset_ids.append(asset_id)
 
             content = get_aiod_asset(asset_id, SupportedAssetType(mapped_asset))
+            url = generate_url(mapped_asset, asset_id)
             if content is None:
                 continue
 
             try:
+                if url is None:
+                    url = content['same_as']
                 new_addition = (
-                    f"name: {content['name']}, publication date:{content['date_published']}, url: {content['same_as']}"  # type: ignore[index]
+                    f"name: {content['name']}, publication date:{content['date_published']}, url: {url}"  # type: ignore[index]
                     f"\ncontent: {content['description']['plain']}\n"  # type: ignore[index]
                 )
                 result += new_addition
